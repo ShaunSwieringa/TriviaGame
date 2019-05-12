1dd695b25d194af266c3ac1ec6b136f56d997efa
Shaun Swieringa <shaun.swieringa@gmail.com>
2019-05-12 16:33:10 -0500

    Finished JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,35 @@
-var game = {
-    correct: 0,
-    incorrect: 0,
-    counter: 45,
-    countdown: function() {
-        game.counter;
-        $('#counter-number').html(game.counter);
-        
-        if (game.counter === 0) {
-            game.done();
-        }
-    }};
+var questionsAnswers = [{
+    question: "What Battle did Tyrion get his scar?",
+    choices: ["Battle of the Blackwater", "Battle of the Bastards", "Battle of the Whispering Wood", "Battle of Hardhome"],
+    correctAnswer: "Battle of the Blackwater"
+
+}, {
+    question: "Whats Bran's Dad's name?",
+    choices: ["Rob Stark", "Ned Stark", "Robert Baratheon", "Tywin Lannister"],
+    correctAnswer: "Ned Stark"
+
+}, {
+    question: "What is the name of Arya's direwolf?",
+    choices: ["Ghost", "Gendry", "Nymeria", "Shadowfax"],
+    correctAnswer: "Nymeria"
+
+}, {
+    question: "How many Stark children are there counting Jon?",
+    choices: ["4", "6", "3", "5"],
+    correctAnswer: "6"
+
+}, {
+    question: "Who was Jon fighting against in the battle of the bastards?",
+    choices: ["Ramsey Bolton", "Roose Bolton", "Walder Frey", "Night King"],
+    correctAnswer: "Ramsey Bolton"
+
+}, {
+    question: "What is Jon Snow's real name?",
+    choices: ["Rhaegar Targaryen", "Viserys Targaryen", "Daenerys Targaryen", "Aegon Targaryen"],
+    correctAnswer: "dil"
+}];
+
+var panel = $('#quiz-area');
 
 $(document).on('click', '#play', function(){
     game.start();
@@ -20,9 +40,21 @@ $(document).on('click', '#play', function(){
      game.done();
  });
 
- start: function() {
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 50,
+    countdown: function() {
+        game.counter--;
+        $('#counter-number').html(game.counter);
+        
+        if (game.counter === 0) { 
+            game.done();
+        }
+    },
+
+start: function() {
   document.getElementById('play').style.display = 'none';
- }
 
 timer = setInterval(game.countdown, 1000);
 
@@ -37,3 +69,59 @@ timer = setInterval(game.countdown, 1000);
     }
 
 panel.append('<button class="btn-primary" id="done">Done</button>');
+},
+
+done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+        if ($(this).val() == questionsAnswers[0].correctAnswer) {
+            game.correct++;
+        } else {
+            game.incorrect++;
+        }
+    });
+    $.each($("input[name='question-1']:checked"), function() {
+        if ($(this).val() == questionsAnswers[1].correctAnswer) {
+            game.correct++;
+        } else {
+            game.incorrect++;
+        }
+    });
+    $.each($("input[name='question-2']:checked"), function() {
+        if ($(this).val() == questionsAnswers[2].correctAnswer) {
+            game.correct++;
+        } else {
+            game.incorrect++;
+        }
+    });
+    $.each($("input[name='question-3']:checked"), function() {
+        if ($(this).val() == questionsAnswers[3].correctAnswer) {
+            game.correct++;
+        } else {
+            game.incorrect++;
+        }
+    });
+    $.each($("input[name='question-4']:checked"), function() {
+        if ($(this).val() == questionsAnswers[4].correctAnswer) {
+            game.correct++;
+        } else {
+            game.incorrect++;
+        }
+    });
+
+    this.result();
+    },
+    result: function() {
+
+        clearInterval(timer);
+         document.getElementById('play').style.display = 'block';
+
+        $('#subwrapper h2').remove();
+        panel.html('<h2>Finished!</h2>');
+        panel.append('<h3>Correct Answers: ' + this.correct + '</h3>');
+        panel.append('<h3>Incorrect Answers: ' + this.incorrect + '</h3>');
+        panel.append('<h3>Unanswered: ' + (questionsAnswers.length - (this.incorrect + this.correct)) + '</h3>');
+    }
+
+};
+
