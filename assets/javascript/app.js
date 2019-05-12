416b81caf684b82189a829101928a1625247542e
Shaun Swieringa <shaun.swieringa@gmail.com>
2019-05-11 23:47:39 -0500

    Added for loop to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 45,
+    countdown: function() {
+        game.counter;
+        $('#counter-number').html(game.counter);
+        
+        if (game.counter === 0) {
+            game.done();
+        }
+    }};
+
 $(document).on('click', '#play', function(){
     game.start();
  });
@@ -15,3 +28,12 @@ timer = setInterval(game.countdown, 1000);
 
  $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">45</span> Seconds</h2>');
         $('#start').remove();
+
+    for (var i = 0; i < questionsAnswers.length; i++) {
+        panel.append('<h2>' + questionsAnswers[i].question + '</h2>');
+        for (var j = 0; j < questionsAnswers[i].choices.length; j++) {
+            panel.append('<p><input type="checkbox" name="question' + '-' + i + '" value="' + questionsAnswers[i].choices[j] + '">' + "     " +questionsAnswers[i].choices[j] + "</p>");
+        }
+    }
+
+panel.append('<button class="btn-primary" id="done">Done</button>');
